6fbfdbfc995028cafa257860659c8eacb64fb9cd
Valeria Terziyska <69038113+ValeriaGTerziyska@users.noreply.github.com>
2021-03-01 10:57:56 +0200

    add not found page

--- routes.js
+++ routes.js
@@ -4,7 +4,10 @@ const aboutController = require("./controllers/aboutController");
 
 const router = Router();
 
-router.use("/", productController);
 router.use("/about", aboutController);
+router.use("/", productController);
+router.get("*", (req, res) => {
+    res.render("404", { layout: false });
+});
 
 module.exports = router;

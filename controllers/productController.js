5d8e3de8e52673a6c6e81c896bc0484a1cca7f0d
Valeria Terziyska <69038113+ValeriaGTerziyska@users.noreply.github.com>
2021-03-01 12:17:43 +0200

    parse form data

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,17 +3,23 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.render("home", {title: "Browse"});
+    res.render("home", { title: "Browse" });
 });
 
 router.get("/create", (req, res) => {
-    res.render("create", {title: "Create"});
+    res.render("create", { title: "Create" });
+});
+
+router.post("/create", (req, res) => {
+    console.log(req.body);
+
+    res.send("created");
 });
 
 router.get("/details/:productId", (req, res) => {
     console.log(req.params.productId);
 
-    res.render("details", {title: "Product Details"});
+    res.render("details", { title: "Product Details" });
 });
 
 module.exports = router;
